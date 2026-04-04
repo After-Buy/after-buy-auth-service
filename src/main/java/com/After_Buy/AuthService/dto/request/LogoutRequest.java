a6b848e6f8375e034b6f6e3dcc7cf3bbd5a13da1
tastysoapwater <gnslwkd13@gachon.ac.kr>
2026-04-04 15:57:27 +0900

    feat(dto): 로그아웃 처리용 Refresh 토큰 래핑 Request 모델 추가

---
+++ src/main/java/com/After_Buy/AuthService/dto/request/LogoutRequest.java
@@ -0,0 +1,21 @@
+package com.After_Buy.AuthService.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 로그아웃 요청 DTO
+ * 사용자가 앱이나 웹에서 로그아웃 시 현재 기기의 Refresh Token을 넘겨주어 무효화시키기 위한 전송 객체
+ *
+ * @since : 2026.04.04
+ * @version : 1.0.0
+ * @author : 신태훈
+ */
+@Getter
+@NoArgsConstructor
+public class LogoutRequest {
+    // 로그아웃으로 폐기할 JWT Refresh Token
+    @NotBlank(message="refresh_token은 필수입니다.")
+    private String refreshToken;
+}
