9838de4276cabc88c4c4ba456ce0379d117c3594
tastysoapwater <gnslwkd13@gachon.ac.kr>
2026-04-04 15:58:34 +0900

    feat(dto): 갱신된 Access Token 정보만을 제공하는 Response 모델 추가

---
+++ src/main/java/com/After_Buy/AuthService/dto/response/TokenRefreshResponse.java
@@ -0,0 +1,24 @@
+package com.After_Buy.AuthService.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * 토큰 재발급 응답 DTO
+ * Refresh Token을 통해 새로 발급된 Access Token의 정보를 담아 반환하는 객체
+ *
+ * @since : 2026.04.04
+ * @version : 1.0.0
+ * @author : 신태훈
+ */
+@Getter
+@Builder
+@AllArgsConstructor
+public class TokenRefreshResponse {
+    // 갱신된 새 Access Token
+    private String accessToken;
+    
+    // 새 Access Token의 만료 시간
+    private long expiresIn;
+}
