fb8a26d53afac3b6155191cf073f2e280a3cd289
tastysoapwater <gnslwkd13@gachon.ac.kr>
2026-04-04 15:57:19 +0900

    feat(dto): Access 토큰 재발급용 Refresh 토큰 래핑 Request 모델 추가

---
+++ src/main/java/com/After_Buy/AuthService/dto/request/TokenRefreshRequest.java
@@ -0,0 +1,21 @@
+package com.After_Buy.AuthService.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Access Token 재발급 요청 DTO
+ * 만료된 Access Token을 갱신하기 위해 클라이언트가 Refresh Token을 서버로 전송하는 객체
+ *
+ * @since : 2026.04.04
+ * @version : 1.0.0
+ * @author : 신태훈
+ */
+@Getter
+@NoArgsConstructor
+public class TokenRefreshRequest {
+    // 갱신을 위해 사용할 기존 Refresh Token
+    @NotBlank(message="refresh_token은 필수입니다.")
+    private String refreshToken;
+}
